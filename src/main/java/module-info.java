11113c56f6fde409414caca195cb37cefece4cde
Jeffrey King'ori <jeffreygithongo@gmail.com>
2021-08-05 15:27:16 +0300

    Fixed javafx SDK import issue

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module edu.strathmore.dsassignment {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
 
     opens edu.strathmore.dsassignment to javafx.fxml;
     exports edu.strathmore.dsassignment;
